bcaa7dfc302bd95ae3db84957207e295a61ac7c5
J Bradshaw <35821275+bshaw334@users.noreply.github.com>
2019-07-25 09:45:45 -0500

    Update cam-jam_lights-flash.py

--- cam-jam_lights-flash.py
+++ cam-jam_lights-flash.py
@@ -1,14 +1,14 @@
-import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO     #this opens the library
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings (False)
+GPIO.setmode(GPIO.BCM)      #pins names
+GPIO.setwarnings (False)    #instructs Python not to print warning
 
-GPIO.setup(18, GPIO.OUT)
+GPIO.setup(18, GPIO.OUT)    #Raspberry Pi pins 18, 23, and 24
 GPIO.setup(23,GPIO.OUT)
 GPIO.setup(24,GPIO.OUT)
 
-print(“Lights on”)
-GPIO.output(18, GPIO.HIGH)
+print(“Lights on”)          #Python command for lights to turn on
+GPIO.output(18, GPIO.HIGH)  #Rpi pins will be given 3.3 volts
 GPIO.output(23, GPIO.HIGH)
 GPIO.output(24, GPIO.HIGH)
 
@@ -21,9 +21,9 @@ GPIO.setup(18, GPIO.OUT)
 GPIO.setup(23, GPIO.OUT)
 GPIO.setup(24, GPIO.OUT)
 
-print(“Lights off”)
-GPIO.output(18, GPIO.LOW)
+print(“Lights off”)         #Python command for lights to turn off
+GPIO.output(18, GPIO.LOW)   #Rpi pins (-) 3.3 volts
 GPIO.output(23, GPIO.LOW)
 GPIO.output(24, GPIO.LOW)
 
-GPIO.cleanup()
+GPIO.cleanup()              #Reset the status of each pins
